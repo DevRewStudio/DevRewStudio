1d6ae72375770a9d590fb05542eb7adea742c729
DevRew Studio <109607537+DevRewStudio@users.noreply.github.com>
2022-07-19 08:29:03 -0600

    Update and rename ImageLoader.cs to LoadImage.cs

--- LoadImage.cs
+++ LoadImage.cs
@@ -4,7 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ImageLoader : MonoBehaviour
+public class LoadImage : MonoBehaviour
 {
     public string url = "https://www.google.com/";
     public Renderer thisRenderer;
