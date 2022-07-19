850ef0813bc5c83503b7558d0de876d52a35f34d
DevRew Studio <109607537+DevRewStudio@users.noreply.github.com>
2022-07-19 08:25:16 -0600

    Create ImageLoader.cs

---
+++ ImageLoader.cs
@@ -0,0 +1,30 @@
+// This Script From Load Image From URL
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageLoader : MonoBehaviour
+{
+    public string url = "https://www.google.com/";
+    public Renderer thisRenderer;
+
+    void Start()
+    {
+        StartCoroutine(LoadFromLikeCoroutine()); 
+
+        thisRenderer.material.color = Color.red;
+    }
+
+    // this section will be run independently
+    private IEnumerator LoadFromLikeCoroutine()
+    {
+        Debug.Log("Loading ....");
+        WWW wwwLoader = new WWW(url);  
+        yield return wwwLoader;        
+
+        Debug.Log("Loaded");
+        thisRenderer.material.color = Color.white;             
+        thisRenderer.material.mainTexture = wwwLoader.texture;  
+    }
+}
